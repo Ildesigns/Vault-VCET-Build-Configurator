93d4862cae931b010f55d40501eb5242776c8d8e
ILIL <ILDesigns@virginmedia.com>
2021-08-27 09:37:12 +0100

    First Build

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Vault-VCET-Build-Configurator
 MS Build Task which configures Vault Extension VCET files to the referenced DLLs automatically.
+ <UsingTask TaskName="SoupSoftware.VCETConfigurator" AssemblyFile="c:\temp\VCETConfigurator.dll">
+
+
+ <Target Name="AfterBuild" AfterTargets="AfterBuild">
+    <VCETConfigurator InputFilename="$(OutputPath)" referencePath="$(AutoDeskSDKSource)" />
+  </Target>
+
---
+++ VCETConfigurator.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using Microsoft.Build.Utilities;
+using System.Xml;
+using Microsoft.Build.Framework;
+using System;
+
+namespace SoupSoftware
+{
+    public class VCETConfigurator : Task
+    {
+
+        [Required]
+        public string Path { get; set; }
+        [Required]
+        public string referencePath { get; set; }
+
+        const string xmlTag = "extension";
+        const string attribute = "interface";
+        public override bool Execute()
+        {
+         
+            if (!System.IO.Directory.Exists(Path))
+            {
+                Log.LogError("Input Path " + Path + " cannot be found");
+                return false;
+            }
+
+            string[] vcetfiles =  System.IO.Directory.GetFiles(Path, "*.VCET.config");
+
+            if (vcetfiles.Length  ==0)
+            {
+                Log.LogMessage("There are 0 files to process");
+                return true;
+            }
+            Log.LogMessage(MessageImportance.Low, "Starting VCET Modification", null);
+           // System.Diagnostics.Debugger.Launch();
+            try
+            {
+                foreach (string vcetFile in vcetfiles)
+                {
+                    try
+                    {
+                        XmlDocument doc = new XmlDocument();
+                        doc.Load(vcetFile);
+                        XmlNode node = null;
+                        if (xmlTag.Length != 0)
+                        {
+
+                            // here we are also searching on an attribute within the node with XPath,
+                            // but not necessarily an attribute we are modifying
+                            node = doc.SelectSingleNode(String.Format("//{0}", xmlTag));
+                        }
+
+                        if (node != null)
+                        {
+                            if (attribute.Length != 0)
+                            {
+                                if (node.Attributes[attribute] != null)
+                                {
+                                    string typeasstring = node.Attributes[attribute].Value;
+                                    string assyname = typeasstring.Split(',')[1].Trim();
+                                    string typefromAssy = typeasstring.Split(',')[0].Trim();
+                                    string assypath = System.IO.Path.Combine(referencePath, assyname + ".dll");
+                                    if (System.IO.File.Exists(assypath))
+                                    {
+                                        System.Reflection.AssemblyName assy = System.Reflection.AssemblyName.GetAssemblyName(assypath);
+                                        string version = assy.Version.ToString();
+                                        string pkt = assy.GetPublicKeyToken().ToString();
+                                        string newinterface = string.Format("{0}, {1}", new string[] { typefromAssy, assy.FullName });
+                                        Log.LogMessage(MessageImportance.High, "Interface is now " + newinterface);
+                                        node.Attributes[attribute].Value = newinterface;
+                                        doc.Save(vcetFile);
+                                        return true;
+                                    }
+                                    else
+                                    {
+                                        Log.LogError("Could not locate file " + assypath);
+                                        return false;
+                                    }
+                                }
+                            }
+                            Log.LogError("Couldn't find Xml Node - {0}", xmlTag);
+                            return false;
+                        }
+                    }
+                    catch (Exception ex2)
+                    {
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.LogError(ex.Message);
+            }
+             return false;
+        }
+    }
+}
+
